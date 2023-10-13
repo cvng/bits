054340d505f310158f35e1235e929dfb8310a851
Cedric Vangout <mail@cvng.dev>
2023-10-13 22:34:08 +0400

    feat: add snapshot testing

--- packages/core/src/commands/comment.rs
+++ packages/core/src/commands/comment.rs
@@ -6,9 +6,14 @@ use bits_data::Comment;
 use bits_data::CommentCreated;
 use bits_data::CommentId;
 use bits_data::Event;
+#[cfg(test)]
+use bits_data::Show;
 use bits_data::ShowId;
 use bits_data::Text;
 use bits_data::UserId;
+#[cfg(test)]
+use insta::assert_json_snapshot;
+use serde::Serialize;
 use thiserror::Error;
 
 #[derive(InputObject)]
@@ -18,7 +23,7 @@ pub struct CommentInput {
   pub text: Text,
 }
 
-#[derive(SimpleObject)]
+#[derive(SimpleObject, Serialize)]
 pub struct CommentPayload {
   pub comment: Comment,
 }
@@ -29,7 +34,7 @@ pub enum Error {
   ShowNotFound(ShowId),
 }
 
-pub async fn comment(input: CommentInput) -> Result<CommentPayload, Error> {
+pub fn comment(input: CommentInput) -> Result<CommentPayload, Error> {
   database::db()
     .shows
     .get(&input.show_id)
@@ -47,4 +52,35 @@ pub async fn comment(input: CommentInput) -> Result<CommentPayload, Error> {
     .ok();
 
   Ok(CommentPayload { comment })
+}
+
+#[test]
+fn test_comment() {
+  database::db().shows.insert(
+    "f5e84179-7f8d-461b-a1d9-497974de10a6".parse().unwrap(),
+    Show {
+      id: "f5e84179-7f8d-461b-a1d9-497974de10a6".parse().unwrap(),
+      creator_id: UserId::new(),
+      name: Text::new("name"),
+      started_at: None,
+    },
+  );
+
+  let payload = comment(CommentInput {
+    user_id: "9ad4e977-8156-450e-ad00-944f9fc730ab".parse().unwrap(),
+    show_id: "f5e84179-7f8d-461b-a1d9-497974de10a6".parse().unwrap(),
+    text: Text::new("text"),
+  })
+  .unwrap();
+
+  assert_json_snapshot!(payload, {".comment.id" => "[uuid]"}, @r###"
+  {
+    "comment": {
+      "id": "[uuid]",
+      "user_id": "9ad4e977-8156-450e-ad00-944f9fc730ab",
+      "show_id": "f5e84179-7f8d-461b-a1d9-497974de10a6",
+      "text": "text"
+    }
+  }
+  "###);
 }
--- packages/data/src/scalars/text.rs
+++ packages/data/src/scalars/text.rs
@@ -1,7 +1,15 @@
 use arrayvec::ArrayString;
 use async_graphql::scalar;
+use serde::Deserialize;
+use serde::Serialize;
 
-#[derive(Copy, Clone, serde::Serialize, serde::Deserialize)]
+#[derive(Copy, Clone, Serialize, Deserialize)]
 pub struct Text(ArrayString<64>);
+
+impl Text {
+  pub fn new(text: &str) -> Self {
+    Self(ArrayString::from(text).unwrap())
+  }
+}
 
 scalar!(Text, "String");
