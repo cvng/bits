5eec1eeb26c91bb9a590dcd2fecaac2cd8ad6c0a
Cedric Vangout <mail@cvng.dev>
2023-10-17 07:36:31 +0400

    feat: init sql

---
+++ docs/schema.sql
@@ -0,0 +1,16 @@
+-- Extensions.
+
+create extension pgcrypto;
+
+-- Domains.
+
+create domain text_min as text check (value != '');
+
+-- Tables.
+
+create table show (
+    id uuid not null default gen_random_uuid() primary key,
+    name text_min not null
+);
+
+alter table show enable row level security;
