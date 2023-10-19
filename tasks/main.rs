c42e40ac7647951363ee9e11fdb2110650a0fccf
Cedric Vangout <mail@cvng.dev>
2023-10-20 01:16:28 +0400

    task

--- tasks/main.rs
+++ tasks/main.rs
@@ -1,12 +1,15 @@
+use std::env;
+use std::process;
+
 fn main() {
   println!(
     "{}",
     String::from_utf8_lossy(
-      &std::process::Command::new("sh")
-        .arg(format!(
-          "./tasks/{}.sh",
-          std::env::args().collect::<Vec<_>>().last().unwrap()
-        ))
+      &process::Command::new("sh")
+        .current_dir("tasks")
+        .arg(env::args().collect::<Vec<_>>().last().unwrap().to_owned() + ".sh")
+        .stdout(process::Stdio::piped())
+        .stderr(process::Stdio::piped())
         .output()
         .unwrap()
         .stdout
