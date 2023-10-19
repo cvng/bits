dade356ab8f349629b3c320083cbd3704c21aa12
Cedric Vangout <mail@cvng.dev>
2023-10-20 02:04:49 +0400

    task

--- tasks/main.rs
+++ tasks/main.rs
@@ -1,18 +1,14 @@
 use std::env;
-use std::process;
+use xshell::cmd;
+use xshell::Shell;
 
-fn main() {
-  println!(
-    "{}",
-    String::from_utf8_lossy(
-      &process::Command::new("sh")
-        .current_dir("tasks")
-        .arg(env::args().collect::<Vec<_>>().last().unwrap().to_owned() + ".sh")
-        .stdout(process::Stdio::piped())
-        .stderr(process::Stdio::piped())
-        .output()
-        .unwrap()
-        .stdout
-    )
-  );
+fn main() -> anyhow::Result<()> {
+  let args = env::args().collect::<Vec<_>>();
+
+  let sh = Shell::new()?;
+  let cmd = format!("tasks/{}.sh", args[1]);
+
+  cmd!(sh, "{cmd}").run()?;
+
+  Ok(())
 }
