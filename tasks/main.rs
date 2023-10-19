29a94a7a298d8da36f5c7f11defc8b40f4a3e8ee
Cedric Vangout <mail@cvng.dev>
2023-10-20 02:18:05 +0400

    task

--- tasks/main.rs
+++ tasks/main.rs
@@ -6,9 +6,9 @@ fn main() -> anyhow::Result<()> {
   let args = env::args().collect::<Vec<_>>();
 
   let sh = Shell::new()?;
-  let cmd = format!("tasks/{}.sh", args[1]);
+  let cmd = &args[1];
 
-  cmd!(sh, "{cmd}").run()?;
+  cmd!(sh, "tasks/{cmd}.sh").run()?;
 
   Ok(())
 }
