8944294806d388480b6b3416af57c658e60d958d
Cedric Vangout <mail@cvng.dev>
2023-10-20 02:22:49 +0400

    task

--- tasks/main.rs
+++ tasks/main.rs
@@ -3,9 +3,9 @@ use xshell::cmd;
 use xshell::Shell;
 
 fn main() -> anyhow::Result<()> {
-  let args = env::args().collect::<Vec<_>>();
-
   let sh = Shell::new()?;
+
+  let args = env::args().collect::<Vec<_>>();
   let cmd = &args[1];
 
   cmd!(sh, "tasks/{cmd}.sh").run()?;
