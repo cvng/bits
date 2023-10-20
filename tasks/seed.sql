f55bbb49b782394386cd817609055dc57ad53042
Cedric Vangout <mail@cvng.dev>
2023-10-20 07:56:49 +0400

    seed

--- tasks/seed.sql
+++ tasks/seed.sql
@@ -5,35 +5,59 @@ truncate table shop.auction cascade;
 truncate table shop.bid cascade;
 truncate table shop.product cascade;
 
-insert into cqrs.event (type, data)
+insert into cqrs.person_created (id, email)
 values
--- noqa: disable=LT05
 (
-  'person_created',
-  '{"id": "00000000-0000-0000-0000-000000000000", "email": "username@test.dev"}'
-),
-(
-  'show_created',
-  '{"id": "00000000-0000-0000-0000-000000000000", "creator_id": "00000000-0000-0000-0000-000000000000", "name": "live_show"}'
-),
-(
-  'product_created',
-  '{"id": "00000000-0000-0000-0000-000000000000", "name": "shop_product"}'
-),
-(
-  'auction_created',
-  '{"id": "00000000-0000-0000-0000-000000000000", "show_id": "00000000-0000-0000-0000-000000000000", "product_id": "00000000-0000-0000-0000-000000000000"}'
-),
-(
-  'bid_created',
-  '{"id": "cd265921-a49d-4650-ac7b-f8614248f147", "auction_id": "00000000-0000-0000-0000-000000000000", "bidder_id": "00000000-0000-0000-0000-000000000000", "amount": 200}'
-),
-(
-  'bid_created',
-  '{"id": "3f2d88d3-9318-4623-8c5b-4d4675318ffd", "auction_id": "00000000-0000-0000-0000-000000000000", "bidder_id": "00000000-0000-0000-0000-000000000000", "amount": 300}'
-),
-(
-  'bid_created',
-  '{"id": "130ad3e1-38ab-4220-8119-2a7872ae7baa", "auction_id": "00000000-0000-0000-0000-000000000000", "bidder_id": "00000000-0000-0000-0000-000000000000", "amount": 400}'
+  '00000000-0000-0000-0000-000000000000',
+  'username@test.dev'
 );
--- noqa: enable=LT05
+
+insert into cqrs.show_created (id, creator_id, name)
+values
+(
+  '00000000-0000-0000-0000-000000000000',
+  '00000000-0000-0000-0000-000000000000',
+  'live_show'
+);
+
+insert into cqrs.product_created (id, name)
+values
+(
+  '00000000-0000-0000-0000-000000000000',
+  'shop_product'
+);
+
+insert into cqrs.auction_created (id, show_id, product_id)
+values
+(
+  '00000000-0000-0000-0000-000000000000',
+  '00000000-0000-0000-0000-000000000000',
+  '00000000-0000-0000-0000-000000000000'
+);
+
+insert into cqrs.bid_created (id, auction_id, bidder_id, amount)
+values
+(
+  'cd265921-a49d-4650-ac7b-f8614248f147',
+  '00000000-0000-0000-0000-000000000000',
+  '00000000-0000-0000-0000-000000000000',
+  200
+);
+
+insert into cqrs.bid_created (id, auction_id, bidder_id, amount)
+values
+(
+  '3f2d88d3-9318-4623-8c5b-4d4675318ffd',
+  '00000000-0000-0000-0000-000000000000',
+  '00000000-0000-0000-0000-000000000000',
+  300
+);
+
+insert into cqrs.bid_created (id, auction_id, bidder_id, amount)
+values
+(
+  '130ad3e1-38ab-4220-8119-2a7872ae7baa',
+  '00000000-0000-0000-0000-000000000000',
+  '00000000-0000-0000-0000-000000000000',
+  400
+);
