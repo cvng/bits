e79f1d61f8a14595659ff39571370ba5f367677e
Cedric Vangout <mail@cvng.dev>
2023-10-22 02:52:27 +0400

    feat(sec): harden policies

--- tasks/migrate.sh
+++ tasks/migrate.sh
@@ -1,23 +1,22 @@
 # https://www.postgresql.org/docs/current/app-psql.html
 
 set -e
-source .env
-
-host="$DATABASE_URL"
 
 # Drop
 
-psql "$host" \
+psql "postgres://postgres:password@localhost:5432/postgres" \
     --no-psqlrc \
     --variable=ON_ERROR_STOP=1 \
     --command="\connect postgres;" \
     --command="drop database if exists bits with (force);" \
     --command="create database bits;" \
+    --quiet
 
 # Init
 
-psql "$host" \
+psql "postgres://postgres:password@localhost:5432/bits" \
     --no-psqlrc \
-    --single-transaction \
     --variable=ON_ERROR_STOP=1 \
+    --command="\connect bits;" \
     --file="docs/schema.sql" \
+    --quiet
--- tasks/seed.sh
+++ tasks/seed.sh
@@ -7,10 +7,17 @@ host="$DATABASE_URL"
 
 # Seed
 
-jq --compact-output ".[]" tasks/seed.json | psql "$host" \
+jq --compact-output ".[]" tasks/seed.json | psql "postgres://postgres:password@localhost:5432/bits" \
     --no-psqlrc \
-    --single-transaction \
     --variable=ON_ERROR_STOP=1 \
+    --command="\connect bits;" \
     --command="create table tmp (row jsonb);" \
     --command="\copy tmp (row) from stdin;" \
+    --quiet
+
+psql "$host" \
+    --no-psqlrc \
+    --variable=ON_ERROR_STOP=1 \
+    --command="\connect bits;" \
+    --command="set role seller;" \
     --command="insert into cqrs.event (type, data) select (row->>'type')::cqrs.event_type, (row->>'data')::jsonb from tmp;" \
