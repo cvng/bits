6fb8ecdf554adc01d81b076d57577dd2db18a027
Cedric Vangout <mail@cvng.dev>
2023-10-21 09:23:48 +0400

    parse event

--- packages/core/src/listener.rs
+++ packages/core/src/listener.rs
@@ -1,20 +1,38 @@
 // https://github.com/launchbadge/sqlx/tree/main/examples/postgres/listen
 
+use serde_json::Value;
 use sqlx::postgres::PgListener;
+use sqlx::postgres::PgNotification;
 
 const NOTIFICATION_CHANNEL: &str = "cqrs.event";
+
+#[derive(Debug, Deserialize)]
+pub struct Event {
+  pub id: i32,
+  pub created: String,
+  pub r#type: String,
+  pub data: Value,
+}
 
 pub async fn listen(
   database_url: &str,
 ) -> Result<(), Box<dyn std::error::Error>> {
   let pool = sqlx::PgPool::connect(database_url).await?;
-
   let mut listener = PgListener::connect_with(&pool).await?;
 
   listener.listen(NOTIFICATION_CHANNEL).await?;
 
   loop {
-    let notification = listener.recv().await?;
-    println!("[from recv]: {notification:?}");
+    handler(listener.recv().await?)?;
   }
+}
+
+fn handler(
+  notification: PgNotification,
+) -> Result<(), Box<dyn std::error::Error>> {
+  let event = serde_json::from_str::<Event>(notification.payload())?;
+
+  println!("{event:#?}");
+
+  Ok(())
 }
