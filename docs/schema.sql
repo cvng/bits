e358b5810ac1da11127a43a709375a48d155f645
Cedric Vangout <mail@cvng.dev>
2023-10-22 08:49:49 +0400

    policies

--- docs/schema.sql
+++ docs/schema.sql
@@ -223,41 +223,64 @@ begin
 end;
 $$ language plpgsql;
 
-create policy comment_create_policy on live.comment for insert to bidder
-with check (author_id = auth.user_id());
+-- Table: cqrs.event
 
-create policy comment_read_policy on live.comment for select to viewer
+create policy event_select_policy on cqrs.event for select to viewer
+using (false);
+
+create policy event_insert_policy on cqrs.event for insert to viewer
+with check (true);
+
+-- Table: auth.person
+
+create policy person_select_policy on auth.person for select to viewer
+using (id = auth.user_id());
+
+create policy person_insert_policy on auth.person for insert to viewer
+with check (id = auth.user_id());
+
+-- Table: live.show
+
+create policy show_select_policy on live.show for select to viewer
 using (true);
 
-create policy show_create_policy on live.show for insert to seller
+create policy show_insert_policy on live.show for insert to seller
 with check (creator_id = auth.user_id());
 
-create policy show_read_policy on live.show for select to viewer
+-- Table: live.comment
+
+create policy comment_select_policy on live.comment for select to viewer
 using (true);
 
-create policy auction_create_policy on shop.auction for insert to seller
+create policy comment_insert_policy on live.comment for insert to bidder
+with check (author_id = auth.user_id());
+
+-- Table: shop.product
+
+create policy product_select_policy on shop.product for select to viewer
+using (true);
+
+create policy product_insert_policy on shop.product for insert to seller
+with check (creator_id = auth.user_id());
+
+-- Table: shop.auction
+
+create policy auction_select_policy on shop.auction for select to viewer
+using (true);
+
+create policy auction_insert_policy on shop.auction for insert to seller
 with check (
   show_id in (select id from live.show where creator_id = auth.user_id()) and
   product_id in (select id from shop.product where creator_id = auth.user_id())
 );
 
-create policy bid_create_policy on shop.bid for insert to bidder
+-- Table: shop.bid
+
+create policy bid_select_policy on shop.bid for select to viewer
+using (true);
+
+create policy bid_insert_policy on shop.bid for insert to bidder
 with check (bidder_id = auth.user_id());
-
-create policy bid_read_policy on shop.bid for select to viewer
-using (true);
-
-create policy product_create_policy on shop.product for insert to seller
-with check (creator_id = auth.user_id());
-
-create policy product_read_policy on shop.product for select to viewer
-using (true);
-
-create policy person_read_policy on auth.person for insert to viewer
-with check (id = auth.user_id());
-
-create policy event_create_policy on cqrs.event for insert to viewer
-with check (true);
 
 --
 -- Triggers
