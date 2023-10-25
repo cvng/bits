6b8a65f48435ad294a99aa2d60bd31d489a074f4
Cedric Vangout <mail@cvng.dev>
2023-10-25 14:25:34 +0400

    codegen

--- tasks/codegen.sh
+++ tasks/codegen.sh
@@ -1,5 +1,5 @@
 # https://github.com/SeaQL/sea-orm/tree/master/sea-orm-cli
 
 sea-orm-cli generate entity \
-    --output-dir packages/graphql/src/entities \
+    --output-dir packages/data/src/entities \
     --seaography
