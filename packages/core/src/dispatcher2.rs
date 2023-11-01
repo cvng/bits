a5538e465908e8d90bb5743cce058d2ba5896221
Cedric Vangout <mail@cvng.dev>
2023-11-01 21:56:30 +0400

    2

--- packages/core/src/dispatcher2.rs
+++ packages/core/src/dispatcher2.rs
@@ -15,8 +15,6 @@ const AUTH_LOGIN_QUERY: &str = "select auth.login($1);";
 const CQRS_EVENT_QUERY: &str =
   "insert into cqrs.event (type, data) values ($1::cqrs.event_type, $2::jsonb)";
 
-type InsertEvent = (String, String);
-
 #[derive(Debug, Error)]
 pub enum ConstraintError {
   #[error("bid validity check failed")]
@@ -39,7 +37,7 @@ pub async fn dispatch(
   client: &Client,
   events: Vec<Event>,
 ) -> Result<Vec<Event>, DispatchError> {
-  let sub = client
+  let user_id = client
     .token
     .as_ref()
     .map(|token| insecure_get_token_sub::<Uuid>(&token.0))
@@ -48,43 +46,36 @@ pub async fn dispatch(
 
   let txn = client.connection.begin().await?;
 
-  if let Some(sub) = sub {
+  if let Some(user_id) = user_id {
     txn
       .execute(Statement::from_sql_and_values(
         DatabaseBackend::Postgres,
         AUTH_LOGIN_QUERY,
-        [sub.into()],
+        [user_id.into()],
       ))
       .await?;
   }
 
-  let json_events = events
-    .iter()
-    .map(|event| {
-      let event = serde_json::to_value(event).unwrap();
-      (
-        event.get("type").unwrap().as_str().unwrap().to_owned(),
-        event.get("payload").unwrap().as_str().unwrap().to_owned(),
-      )
-    })
-    .collect::<Vec<InsertEvent>>();
+  for event in &events {
+    let event = serde_json::to_value(event)?;
+    let event_type = event.get("type").unwrap().as_str().unwrap();
+    let event_payload = event.get("payload").unwrap().as_str().unwrap();
 
-  for (type_, payload) in json_events {
     let res = txn
       .execute(Statement::from_sql_and_values(
         DatabaseBackend::Postgres,
         CQRS_EVENT_QUERY,
-        [type_.into(), payload.into()],
+        [event_type.into(), event_payload.into()],
       ))
       .await;
 
     if let Err(DbErr::Exec(RuntimeErr::SqlxError(
       sqlx::error::Error::Database(e),
-    ))) = &res
+    ))) = res
     {
       match to_constraint_err(e) {
         Some(err) => return Err(DispatchError::Constraint(err)),
-        None => return Err(DispatchError::Database(res.err().unwrap())),
+        None => unimplemented!(), // TODO: return Err(DispatchError::Database(res.err().unwrap())),
       }
     }
   }
@@ -94,7 +85,7 @@ pub async fn dispatch(
   Ok(events)
 }
 
-fn to_constraint_err(err: &Box<dyn DatabaseError>) -> Option<ConstraintError> {
+fn to_constraint_err(err: Box<dyn DatabaseError>) -> Option<ConstraintError> {
   match err.constraint() {
     Some("bid_validity_check") => Some(ConstraintError::BidValidityCheck),
     _ => None,
