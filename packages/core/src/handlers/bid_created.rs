9d5568d6824652519d2bc31e4fb3acd79affa493
Cedric Vangout <mail@cvng.dev>
2023-11-01 15:46:20 +0400

    login

--- packages/core/src/handlers/bid_created.rs
+++ packages/core/src/handlers/bid_created.rs
@@ -4,6 +4,10 @@ use crate::error::Error;
 use crate::error::Result;
 use crate::Client;
 use bits_data::BidCreated;
+use sea_orm::ConnectionTrait;
+use sea_orm::DatabaseBackend;
+use sea_orm::Statement;
+use sea_orm::TransactionTrait;
 use uuid::Uuid;
 
 pub async fn bid_created(client: &Client, event: BidCreated) -> Result<()> {
@@ -14,9 +18,29 @@ pub async fn bid_created(client: &Client, event: BidCreated) -> Result<()> {
     .ok_or(Error::NotFound(event.bid.id))
     .ok();
 
-  if let Some(token) = &client.token {
-    dbg!(insecure_get_token_sub::<Uuid>(token.0.as_str())?);
-  }
+  let sub =
+    insecure_get_token_sub::<Uuid>(client.token.as_ref().unwrap().0.as_str())?
+      .unwrap();
+
+  let txn = client.connection.begin().await.unwrap();
+
+  txn
+    .execute(Statement::from_sql_and_values(
+      DatabaseBackend::Postgres,
+      "select auth.login($1);",
+      [sub.into()],
+    ))
+    .await
+    .unwrap();
+
+  txn
+    .execute(Statement::from_sql_and_values(
+      DatabaseBackend::Postgres,
+      "insert into cqrs.event (type, data) values ($1::cqrs.event_type, $2::jsonb)",
+      ["bid_created".into(), serde_json::to_value(&event.bid).unwrap().into()]
+    )).await.unwrap();
+
+  txn.commit().await.unwrap();
 
   Ok(())
 }
--- tasks/bin.rs
+++ tasks/bin.rs
@@ -1,7 +1,7 @@
 use std::env;
 use xshell::cmd;
-use xshell::Shell;
 use xshell::Result;
+use xshell::Shell;
 
 fn main() -> Result<()> {
   let sh = Shell::new()?;
