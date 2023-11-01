2aa4ca217b431452c8b7409744dd73b61936d66f
Cedric Vangout <mail@cvng.dev>
2023-11-01 16:32:36 +0400

    to_constraint_err

--- packages/core/src/handlers/bid_created.rs
+++ packages/core/src/handlers/bid_created.rs
@@ -6,8 +6,11 @@ use crate::Client;
 use bits_data::BidCreated;
 use sea_orm::ConnectionTrait;
 use sea_orm::DatabaseBackend;
+use sea_orm::DbErr;
+use sea_orm::RuntimeErr;
 use sea_orm::Statement;
 use sea_orm::TransactionTrait;
+use sqlx::error::DatabaseError;
 use uuid::Uuid;
 
 pub async fn bid_created(client: &Client, event: BidCreated) -> Result<()> {
@@ -33,14 +36,34 @@ pub async fn bid_created(client: &Client, event: BidCreated) -> Result<()> {
     .await
     .unwrap();
 
-  txn
+  let res = txn
     .execute(Statement::from_sql_and_values(
       DatabaseBackend::Postgres,
       "insert into cqrs.event (type, data) values ($1::cqrs.event_type, $2::jsonb)",
       ["bid_created".into(), serde_json::to_value(&event.bid).unwrap().into()]
-    )).await.unwrap();
+    )).await;
+
+  if let Err(DbErr::Exec(RuntimeErr::SqlxError(
+    sqlx::error::Error::Database(e),
+  ))) = &res
+  {
+    dbg!(to_constraint_err(e.as_ref()));
+    // res.unwrap();
+  }
 
   txn.commit().await.unwrap();
 
   Ok(())
+}
+
+#[derive(Debug)]
+enum ConstraintError {
+  BidValidityCheck,
+}
+
+fn to_constraint_err(err: &dyn DatabaseError) -> Option<ConstraintError> {
+  match err.constraint() {
+    Some("bid_validity_check") => Some(ConstraintError::BidValidityCheck),
+    _ => None,
+  }
 }
