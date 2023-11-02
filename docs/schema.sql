893a776bce6e684b9f994bf7ce5c90a514230954
Cedric Vangout <mail@cvng.dev>
2023-11-02 15:13:35 +0400

    cqrs

--- docs/schema.sql
+++ docs/schema.sql
@@ -101,18 +101,6 @@ create type cqrs.show_created as (
 -- Tables
 --
 
--- Table: cqrs.event
-
-create table cqrs.event (
-  id bigint not null primary key generated always as identity,
-  created timestamptz not null default clock_timestamp(),
-  user_id id not null references auth.person (id),
-  type cqrs.event_type not null,
-  data jsonb not null
-);
-
-alter table cqrs.event enable row level security;
-
 -- Table: auth.person
 
 create table auth.person (
@@ -124,6 +112,18 @@ create table auth.person (
 );
 
 alter table auth.person enable row level security;
+
+-- Table: cqrs.event
+
+create table cqrs.event (
+  id bigint not null primary key generated always as identity,
+  created timestamptz not null default clock_timestamp(),
+  user_id id not null references auth.person (id),
+  type cqrs.event_type not null,
+  data jsonb not null
+);
+
+alter table cqrs.event enable row level security;
 
 -- Table: live.show
 
