ee3dfef1db6f959a0c0da699b32e76bb574ec1f8
Cedric Vangout <mail@cvng.dev>
2023-11-02 13:26:28 +0400

    fix: strict mode for psql

--- tasks/db-seed.sh
+++ tasks/db-seed.sh
@@ -11,21 +11,13 @@ host="$DATABASE_URL"
 name="bits"
 file="tasks/data/events.json"
 
-psql "$host" \
-<<SQL
-\connect $name;
+psql "$host" --set=ON_ERROR_STOP=true --command="create table tmp (row jsonb);"
 
-create table if not exists tmp (row jsonb);
-grant select on tmp to public;
-SQL
-
-jq --compact-output ".[]" "$file" | psql "$host" \
+jq --compact-output ".[]" "$file" | psql "$host" --set=ON_ERROR_STOP=true \
     --command="\copy tmp (row) from stdin;"
 
-psql "$host" \
+psql "$host" --set=ON_ERROR_STOP=true \
 <<SQL
-\connect $name;
-
 insert into auth.person (id, email, role)
 values ('00000000-0000-0000-0000-000000000000', 'admin@test.dev', 'admin');
 
@@ -33,16 +25,19 @@ do \$$
 declare
     event jsonb;
 begin
-for event in select row from tmp loop
-    perform auth.login((event->>'user')::id);
+    for event in select row from tmp loop
+        perform auth.login((event->>'user')::id);
 
-    insert into cqrs.event (type, data)
-    values ((event->>'type')::cqrs.event_type, (event->>'data')::jsonb);
-end loop;
+        insert into cqrs.event (type, data)
+        values ((event->>'type')::cqrs.event_type, (event->>'data')::jsonb);
+    end loop;
 end; \$$;
 
-do \$$ begin perform auth.login('00000000-0000-0000-0000-000000000000'); end; \$$;
+do \$$
+begin
+    perform auth.login('00000000-0000-0000-0000-000000000000');
+end; \$$;
 select id, created, type, data->>'id' as "data.id" from cqrs.event;
 SQL
 
-psql "$host" --command="drop table tmp;"
+psql "$host" --set=ON_ERROR_STOP=true --command="drop table tmp;"
