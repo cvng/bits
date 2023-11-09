dd5e11426818a5fd5afbfe9d63d25e43d7b7fc3e
Cedric Vangout <mail@cvng.dev>
2023-11-09 16:00:19 +0400

    normalize import

--- docs/schema.sql
+++ docs/schema.sql
@@ -1,32 +1,44 @@
-\include_relative schema/schemas.sql
+\include_relative schema/main.sql
 \include_relative schema/roles.sql
 \include_relative schema/domains.sql
 
 \include_relative schema/auth/auth_enums.sql
-\include_relative schema/cqrs/cqrs_enums.sql
-
-\include_relative schema/auth/auth_tables.sql
-\include_relative schema/cqrs/cqrs_tables.sql
-\include_relative schema/live/live_tables.sql
-\include_relative schema/shop/shop_tables.sql
-
-\include_relative schema/live/live_checks.sql
-\include_relative schema/shop/shop_checks.sql
-
-\include_relative schema/cqrs/cqrs_types.sql
-
-\include_relative schema/auth/auth_perms.sql
-\include_relative schema/cqrs/cqrs_perms.sql
-\include_relative schema/live/live_perms.sql
-\include_relative schema/shop/shop_perms.sql
-
+\include_relative schema/auth/auth_types.sql
 \include_relative schema/auth/auth_utils.sql
-
+\include_relative schema/auth/auth_tables.sql
+\include_relative schema/auth/auth_checks.sql
+\include_relative schema/auth/auth_perms.sql
 \include_relative schema/auth/auth_policies.sql
-\include_relative schema/cqrs/cqrs_policies.sql
-\include_relative schema/live/live_policies.sql
-\include_relative schema/shop/shop_policies.sql
+\include_relative schema/auth/auth_triggers.sql
+\include_relative schema/auth/auth_handlers.sql
 
+\include_relative schema/live/live_enums.sql
+\include_relative schema/live/live_types.sql
+\include_relative schema/live/live_utils.sql
+\include_relative schema/live/live_tables.sql
+\include_relative schema/live/live_checks.sql
+\include_relative schema/live/live_perms.sql
+\include_relative schema/live/live_policies.sql
+\include_relative schema/live/live_triggers.sql
+\include_relative schema/live/live_handlers.sql
+
+\include_relative schema/shop/shop_enums.sql
+\include_relative schema/shop/shop_types.sql
+\include_relative schema/shop/shop_utils.sql
+\include_relative schema/shop/shop_tables.sql
+\include_relative schema/shop/shop_checks.sql
+\include_relative schema/shop/shop_perms.sql
+\include_relative schema/shop/shop_policies.sql
+\include_relative schema/shop/shop_triggers.sql
+\include_relative schema/shop/shop_handlers.sql
+
+\include_relative schema/cqrs/cqrs_enums.sql
+\include_relative schema/cqrs/cqrs_types.sql
+\include_relative schema/cqrs/cqrs_utils.sql
+\include_relative schema/cqrs/cqrs_tables.sql
+\include_relative schema/cqrs/cqrs_checks.sql
+\include_relative schema/cqrs/cqrs_perms.sql
+\include_relative schema/cqrs/cqrs_policies.sql
 \include_relative schema/cqrs/cqrs_triggers.sql
 \include_relative schema/cqrs/cqrs_handlers.sql
 
