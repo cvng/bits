f2c5863430a44b401ca23fa20d033515287a4d5f
Cedric Vangout <mail@cvng.dev>
2023-11-09 19:58:31 +0400

    --

--- docs/schema.sql
+++ docs/schema.sql
@@ -1,10 +1,6 @@
---
-
 \include_relative schema/roles.sql
 \include_relative schema/schemas.sql
 \include_relative schema/domains.sql
-
---
 
 \include_relative schema/auth/person.sql
 \include_relative schema/auth/include/auth_utils.sql
@@ -21,8 +17,6 @@
 \include_relative schema/cqrs/include/cqrs_utils.sql
 \include_relative schema/cqrs/include/cqrs_triggers.sql
 \include_relative schema/cqrs/include/cqrs_handlers.sql
-
---
 
 \include_relative schema/permissions.sql
 \include_relative schema/views.sql
