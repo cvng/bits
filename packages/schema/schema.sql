bbfbeb3c7ef321581fb40f48042168c4472824a0
Cedric Vangout <mail@cvng.dev>
2023-11-11 00:33:57 +0400

    mv

--- packages/schema/schema.sql
+++ packages/schema/schema.sql
@@ -1,25 +1,25 @@
-\include_relative sql/roles.sql
-\include_relative sql/schemas.sql
-\include_relative sql/domains.sql
+\include_relative src/roles.sql
+\include_relative src/schemas.sql
+\include_relative src/domains.sql
 
-\include_relative sql/auth/person.sql
-\include_relative sql/auth/include/auth_utils.sql
+\include_relative src/auth/person.sql
+\include_relative src/auth/include/auth_utils.sql
 
-\include_relative sql/live/show.sql
-\include_relative sql/live/comment.sql
+\include_relative src/live/show.sql
+\include_relative src/live/comment.sql
 
-\include_relative sql/shop/product.sql
-\include_relative sql/shop/auction.sql
-\include_relative sql/shop/auction_session.sql
-\include_relative sql/shop/bid.sql
-\include_relative sql/shop/config.sql
-\include_relative sql/shop/include/shop_utils.sql
+\include_relative src/shop/product.sql
+\include_relative src/shop/auction.sql
+\include_relative src/shop/auction_session.sql
+\include_relative src/shop/bid.sql
+\include_relative src/shop/config.sql
+\include_relative src/shop/include/shop_utils.sql
 
-\include_relative sql/cqrs/event.sql
-\include_relative sql/cqrs/include/cqrs_utils.sql
-\include_relative sql/cqrs/include/cqrs_triggers.sql
-\include_relative sql/cqrs/include/cqrs_handlers.sql
+\include_relative src/cqrs/event.sql
+\include_relative src/cqrs/include/cqrs_utils.sql
+\include_relative src/cqrs/include/cqrs_triggers.sql
+\include_relative src/cqrs/include/cqrs_handlers.sql
 
-\include_relative sql/permissions.sql
-\include_relative sql/policies.sql
-\include_relative sql/views.sql
+\include_relative src/permissions.sql
+\include_relative src/policies.sql
+\include_relative src/views.sql
--- scripts/sql-check.sh
+++ scripts/sql-check.sh
@@ -4,11 +4,11 @@ set -eu -o pipefail
 # https://squawkhq.com/docs/cli
 
 npx squawk-cli \
-    packages/schema/sql/*.sql \
-    packages/schema/sql/**/*.sql \
-    packages/schema/sql/**/include/*.sql \
+    packages/schema/src/*.sql \
+    packages/schema/src/**/*.sql \
+    packages/schema/src/**/**/*.sql \
 
 
 # https://docs.sqlfluff.com/en/stable/gettingstarted.html
 
-sqlfluff fix packages/schema/sql
+sqlfluff fix packages/schema/src
