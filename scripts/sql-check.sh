777f707f20a1f28d19ed0da974ddf29553bff0a1
Cedric Vangout <mail@cvng.dev>
2023-11-11 01:02:15 +0400

    sql-check

--- scripts/sql-check.sh
+++ scripts/sql-check.sh
@@ -3,12 +3,8 @@ set -eu -o pipefail
 
 # https://squawkhq.com/docs/cli
 
-npx squawk-cli \
-    packages/schema/src/*.sql \
-    packages/schema/src/**/*.sql \
-    packages/schema/src/**/**/*.sql \
-
+npx squawk-cli $(find packages/schema/src -name '*.sql' ! -name 'lib.sql')
 
 # https://docs.sqlfluff.com/en/stable/gettingstarted.html
 
-sqlfluff fix packages/schema/src
+sqlfluff fix --quiet --disable-progress-bar packages/schema/src
