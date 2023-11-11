bc75ce846c837b10a7b8813f7f201ee87bf0b6b8
Cedric Vangout <mail@cvng.dev>
2023-11-11 17:04:11 +0400

    authenticator

--- packages/schema/src/roles.sql
+++ packages/schema/src/roles.sql
@@ -1,6 +1,7 @@
 -- Roles
 
-create role viewer noinherit;
+create role viewer;
 create role bidder in role viewer;
 create role seller in role bidder;
 create role admin in role seller;
+create role authenticator in role admin login noinherit;
