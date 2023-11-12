322bbe6e6316710a622e6b123f8583b1f449ffbb
Cedric Vangout <mail@cvng.dev>
2023-11-12 17:27:38 +0400

    fix(app): reset auction timeout (#77)

--- packages/schema/src/jobs/schedule.sql
+++ packages/schema/src/jobs/schedule.sql
@@ -4,5 +4,5 @@ create function jobs.schedule() returns void
 language plpgsql as $$
 begin
   perform cron.schedule(
-    'check_expired_auctions', '2 seconds', 'select jobs.check_expired_auctions()');
+    'check-expired-auctions', '2 seconds', 'select jobs.check_expired_auctions()');
 end; $$;
