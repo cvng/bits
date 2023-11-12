21b53d23271c4f5361297c1448096946fe754dd6
Cedric Vangout <mail@cvng.dev>
2023-11-13 02:56:39 +0400

    command

--- packages/core/src/commands/mod.rs
+++ packages/core/src/commands/mod.rs
@@ -4,16 +4,3 @@ pub mod create_auction;
 pub mod create_product;
 pub mod create_show;
 pub mod start;
-
-/// Generic command trait.
-pub trait Command {
-  type Error;
-  type Event;
-  type Input;
-  type Result;
-
-  fn handle(&self, input: Self::Input)
-    -> Result<Vec<Self::Event>, Self::Error>;
-
-  fn apply(events: Vec<Self::Event>) -> Option<Self::Result>;
-}
