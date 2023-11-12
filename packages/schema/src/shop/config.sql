15ef5fbec0b367aaee02156a89403ef29df5428e
Cedric Vangout <mail@cvng.dev>
2023-11-12 16:25:09 +0400

    2 seconds

--- packages/schema/src/shop/config.sql
+++ packages/schema/src/shop/config.sql
@@ -1,8 +1,8 @@
 -- Table: shop.config
 
 create table shop.config (
-  auction_timeout_secs interval not null default '1 secs',
-  auction_refresh_secs interval not null default '1 secs'
+  auction_timeout_secs interval not null default '60 seconds',
+  auction_refresh_secs interval not null default '15 seconds'
 );
 
 alter table shop.config enable row level security;
